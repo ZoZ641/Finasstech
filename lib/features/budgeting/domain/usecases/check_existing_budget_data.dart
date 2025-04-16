eb694c1d666ea17289cb775d76ca3e61b1b4ae6f
ZoZ641 <zyadhassan641@gmail.com>
2025-04-16 20:39:41 +0200

    update on CheckExistingBudgetData UseCase to remove userId from it since there is the data of only 1 user

--- lib/features/budgeting/domain/usecases/check_existing_budget_data.dart
+++ lib/features/budgeting/domain/usecases/check_existing_budget_data.dart
@@ -4,13 +4,15 @@ import '../../../../core/error/failures.dart';
 import '../../../../core/usecase/usecase.dart';
 import '../repository/budget_repository.dart';
 
-class CheckExistingBudgetData implements UseCase<bool, String> {
+class CheckExistingBudgetData implements UseCase<bool, NoParams> {
   final BudgetRepository budgetRepository;
 
   const CheckExistingBudgetData(this.budgetRepository);
 
   @override
-  Future<Either<Failure, bool>> call(String userId) async {
+  Future<Either<Failure, bool>> call(NoParams params) async {
     return await budgetRepository.hasExistingBudgetData();
   }
 }
+
+class NoParams {}
