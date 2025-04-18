b2170b3bc24013d1b6cabc91906cf706afe33f33
ZoZ641 <zyadhassan641@gmail.com>
2025-04-18 20:03:58 +0200

    updates the entites in budget feature

--- lib/features/budgeting/domain/entities/budget.dart
+++ lib/features/budgeting/domain/entities/budget.dart
@@ -1,30 +1,14 @@
-import 'package:hive_ce/hive.dart';
-
 import 'budget_category.dart';
 
-@HiveType(typeId: 2)
-class Budget extends HiveObject {
-  @HiveField(0)
+class Budget {
   final String id;
-
-  /*@HiveField(1)
-  final String userId;*/
-
-  @HiveField(1)
   final double forecastedSales;
-
-  @HiveField(2)
   final Map<String, BudgetCategory> categories;
-
-  @HiveField(3)
   final DateTime createdAt;
-
-  @HiveField(4)
   final DateTime updatedAt;
 
   Budget({
     required this.id,
-    //required this.userId,
     required this.forecastedSales,
     required this.categories,
     required this.createdAt,
